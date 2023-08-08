32c7b6f86fd9b66f670ff282afeebc6e9a8d0ae5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-08 08:15:03 +0000

    Task4 Add kwargs, Create Object from Dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,23 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Define a BaseModel Class"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
+        if len(kwargs) == 0:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -17,7 +26,7 @@ class BaseModel():
     def save(self):
         """update the public instance updated_at"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """returns the dictionary representation of the instance"""
         todict = self.__dict__
