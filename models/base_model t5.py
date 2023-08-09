76bae4a9d15ad28bac30f121811609c691700800
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-09 08:21:30 +0000

    Task6 Console 0.0.1

---
+++ models/base_model t5.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+class BaseModel that defines all common
+attributes/methods for other classes
+take care of the initialization, serialization and
+deserialization of your future instances
+"""
+from uuid import uuid4
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """
+    class BaseModel that defines all common
+    attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialization of BaseModel Class"""
+        #if len(kwargs) == 0:
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        #else:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]: #TODO:mandatory atribut
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            storage.new(self)
+        
+    def __str__(self) -> str:
+        """Returns the string representation of an instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self) -> None:
+        """update the public instance updated_at"""
+        self.updated_at = datetime.now()
+        storage.save() #TODO:updated_att will not be saved
+        
+
+    def to_dict(self) -> dict:
+        """returns the dictionary representation of the instance"""
+        todict = dict(self.__dict__)
+        todict["__class__"] = self.__class__.__name__
+        if not isinstance(todict["created_at"], str):
+            todict["created_at"] = todict["created_at"].isoformat()
+        if not isinstance(todict["updated_at"], str):
+            todict["updated_at"] = todict["updated_at"].isoformat()
+        return todict
