3f6f8502c2e382c18de39d9198446bc0955bf21d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-13 10:27:09 +0000

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -100,11 +100,6 @@ class BaseModelTestCase(unittest.TestCase):
         self.assertTrue(hasattr(new2, "save"))
         self.assertTrue(hasattr(new2, "to_dict"))
 
-        """existince"""
-        self.assertTrue(hasattr(new2, "id"))
-        self.assertTrue(hasattr(new2, "created_at"))
-        self.assertTrue(hasattr(new2, "updated_at"))
-        self.assertFalse(hasattr(new2, "__class__"))
 
         ############################
 
